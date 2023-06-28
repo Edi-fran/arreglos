5e939dc8fd6d343da837f2ccf6c05c7ef4f62261
Edi-fran <135551783+Edi-fran@users.noreply.github.com>
2023-06-27 19:47:47 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,40 @@
+function verificar()
+{
+    let variable1=document.getElementById('a').checked;
+    alert (variable1);
+
+}
+
+//function ingresar_datosArray()
+//{
+  //  var arreglo_ingreso =new Array();
+    //var valore_arreglo= getElementByClassName('ingreso'),namesValues=[].map.call(valore_arreglo,function(dataInput)
+  //{
+    //    arreglo_ingreso.push(dataInput.value);
+  //});
+  //arreglo_ingreso.array.forEach(function(inputsValuesData)
+  //{
+    //alert("el ato es :"+ valore_arreglo)
+  //});
+//}
+function recoger(){
+    var arreglo_ingreso =new Array();
+    let dato=document.getElementById('ingreso').value;
+    arreglo_ingreso.push(dato);
+
+}
+function mostrar()
+{
+
+
+   //document.write (arreglo_ingreso.mostrar.value);
+  //  for(var i=0;i<arreglo_ingreso;i++)
+  //arreglo_ingreso.forEach(element => {
+     //document.write(element);
+     
+
+  }
+  //);
+   
+//}
+
